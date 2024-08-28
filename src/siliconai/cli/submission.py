7c1fc783f3f7f9154c765999af3ac5faa6aaa274
Tadej Novak <tadej@tano.si>
2024-08-28 07:26:07 +0200

    Minor slurm tweaks

--- src/siliconai/cli/submission.py
+++ src/siliconai/cli/submission.py
@@ -21,6 +21,7 @@ def create_slurm_submission_script(
     if not output_dir.exists():
         output_dir.mkdir(parents=True)
 
+    job_name = f"SiliconAI_{config.output_name}_{config.run_number()}"
     output_file = config.output_path / f"run_{config.run_number()}" / "submit.sh"
     output_log = config.output_path / f"run_{config.run_number()}" / "slurm.log"
     duration = 23  # TODO: make configurable
@@ -29,14 +30,16 @@ def create_slurm_submission_script(
         file.write(
             "#!/bin/bash\n"
             "\n"
+            f'#SBATCH --job-name="{job_name}"\n'
             "#SBATCH --partition=gpu\n"
             f"#SBATCH --nodes={n_node}\n"
             f"#SBATCH --gres=gpu:{n_gpu}\n"
             f"#SBATCH --ntasks-per-node={n_gpu}\n"
             "#SBATCH --cpus-per-task=32\n"
-            "#SBATCH --mem=0\n"
+            f"#SBATCH --mem={32*n_gpu}GB\n"
             f"#SBATCH --time=0-{duration}:00:00\n"
             f"#SBATCH --output={output_log}\n"
+            "#SBATCH --signal=INT@60\n"
             "\n"
             "# debugging flags\n"
             "# export NCCL_DEBUG=INFO\n"
