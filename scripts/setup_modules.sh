8346658e3d56e3bf3b1b7ac69d6a8d9608932848
Tadej Novak <tadej.novak@cern.ch>
2024-11-26 17:24:10 +0100

    Move from pdm to uv

--- scripts/setup_modules.sh
+++
@@ -1,8 +0,0 @@
-# A script to setup Vega modules
-
-source "/etc/profile.d/00-modulepath.sh"
-source "/etc/profile.d/modules.sh"
-source "/ceph/hpc/software/cvmfs_env.sh"
-
-module load Python/3.11
-module unload OpenSSL/1.1
